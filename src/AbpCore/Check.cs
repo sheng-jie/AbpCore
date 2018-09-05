e433645b45727575f9fec411e4520ab97b38073b
shengjie_yan <ysjshengjie@live.cn>
2018-09-05 14:29:41 +0800

    Add Microsoft.Extensions.DependencyInjection, JetBrains.Annotations Nuget Package

---
+++ src/AbpCore/Check.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using AbpCore.Modularity;
+
+namespace AbpCore
+{
+    public static class Check
+    {
+        public static T NotNull<T>(T value, string parameterName)
+        {
+            if (value == null)
+            {
+                throw new ArgumentNullException(parameterName);
+            }
+
+            return value;
+        }
+    }
+}

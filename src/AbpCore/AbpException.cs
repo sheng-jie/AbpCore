52e9ddbf8b4166e229918ede8b1f95c2fa053620
shengjie_yan <ysjshengjie@live.cn>
2018-09-05 12:22:39 +0800

    Add abp exception

---
+++ src/AbpCore/AbpException.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Runtime.Serialization;
+
+namespace AbpCore
+{
+    [Serializable]
+    public class AbpException : Exception
+    {
+        //
+        // For guidelines regarding the creation of new exception types, see
+        //    http://msdn.microsoft.com/library/default.asp?url=/library/en-us/cpgenref/html/cpconerrorraisinghandlingguidelines.asp
+        // and
+        //    http://msdn.microsoft.com/library/default.asp?url=/library/en-us/dncscol/html/csharp07192001.asp
+        //
+
+        public AbpException()
+        {
+        }
+
+        public AbpException(string message) : base(message)
+        {
+        }
+
+        public AbpException(string message, Exception inner) : base(message, inner)
+        {
+        }
+
+        protected AbpException(
+            SerializationInfo info,
+            StreamingContext context) : base(info, context)
+        {
+        }
+    }
+}
